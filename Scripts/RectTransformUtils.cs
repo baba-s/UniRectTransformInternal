d5d1c9c1fa0698f9067ef045069e20584a6b4e5c
baba_s <shotababa1206@gmail.com>
2020-10-05 19:36:56 +0900

    first commit

---
+++ Scripts/RectTransformUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace Kogane
+{
+	public static class RectTransformUtils
+	{
+		public static DrivenTransformProperties GetDrivenProperties( RectTransform self )
+		{
+			var type             = typeof( RectTransform );
+			var propertyInfo     = type.GetProperty( "drivenProperties", BindingFlags.Instance | BindingFlags.NonPublic );
+			var drivenProperties = ( DrivenTransformProperties ) propertyInfo.GetValue( self );
+
+			return drivenProperties;
+		}
+	}
+}
